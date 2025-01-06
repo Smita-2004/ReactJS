70841a9242f81684c952c51a7086b49bbc45a165
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-01-06 20:11:15 +0530

    Add files via upload

---
+++ router/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Profile = ()=>{
+    const {userId} = useParams();
+    return(
+        <div>
+            <h1>Profile Page</h1>
+            <p>User ID:{userId}</p>
+        </div>
+    );
+}
+export default Profile;
