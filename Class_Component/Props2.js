2820c06b9b530857e426419039f1ceb0e8b6a408
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-01-17 10:27:53 +0530

    Add files via upload

---
+++ Class_Component/Props2.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+class Props2 extends Component{
+    render(){
+        return <h1>Hello {this.props.stdname} , Welcome</h1>
+    }
+}
+export default Props2
