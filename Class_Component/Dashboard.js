5b0df46e99ce5eda950344c20d9b7025fec91be3
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-01-17 10:26:57 +0530

    Add files via upload

---
+++ Class_Component/Dashboard.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+class Dashboard extends Component{
+    render(){
+        return <h1>Hello World! This is Dashboard</h1>;
+    }
+}
+export default Dashboard
